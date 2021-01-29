45364cc0f097936a971bb53ee63052269a3b3d37
Herka Trya Fitra <herkahahaha@gmail.com>
2021-01-29 07:43:00 +0700

    adding Nunito fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,42 @@
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Nunito"],
+        serif: ["Nunito"],
+        mono: ["SFMono-Regular", "Menlo"],
+        nunito: ["Nunito"],
+      },
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
+        dark: {
+          300: "#2D303E",
+          500: "#252836",
+          900: "#1F1D2B",
+        },
+        light: {
+          100: "#F4F5F9",
+        },
       },
       spacing: {
-        28: '7rem',
+        28: "7rem",
       },
       letterSpacing: {
-        tighter: '-.04em',
+        tighter: "-.04em",
       },
       lineHeight: {
         tight: 1.2,
       },
       fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
       },
       boxShadow: {
-        sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        md: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
+        md: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
     },
   },
-}
+};
